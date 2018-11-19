ad2da7e4f3e42230499f5ef03264812d9bd27434
Jack Lu <jlu2025@gmail.com>
2018-11-19 01:54:20 -0500

    finish

--- forking.c
+++ forking.c
@@ -3,27 +3,49 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
-//#include <signal.h>
 
 int main(){
 
-    int pid = getpid();
-    //printf("ppid : %i and pid : %i\n", getppid(), getpid());
-    //printf("PARENT : %i\n", getpid());
-    //printf("%i\n",errno);
-    //perror("errno");
+    //printf("F = %i, my pid = %i, my parent is %i\n", f, getpid(), getppid());
+    //if (f == -1){
+    //    printf("error\n");
+    //}else if (f){
+    //    printf("I am parent my pid = %i\n", getpid());
+    //    f = fork();
+    //}else{
+    //    printf("I am child of %i, my pid is %i\n", getppid(), getpid());
+    //}
 
-    if (getpid() == pid){
-        printf("I am parent %i\n", pid);
-        int child0 = fork();
-        int child1 = fork();
-    }else{
-        printf("I am child %i\n", getpid());
+
+    printf("I am parent %i, before fork\n", getpid());
+    int f = fork();
+    if (f){
+        if (f = fork()){
+            // parent
+            //perror("error: ");
+        } else {
+            // second child
+            //perror("error: ");
+        }
+    } else{
+        // first child
+        //perror("error: ");
     }
 
-    //perror("errno");
-
-    //printf("I AM %i\n", getpid());
+    if (f){
+        int status;
+        int dead_kid = wait(&status);
+        //perror("error: ");
+        printf("Child %i woke up and died, slept for %i secs\n", dead_kid, WEXITSTATUS(status));
+    } else{
+        srand(getpid());
+        int s = rand() % (20 + 1 - 5) + 5;
+        printf("Child %i now sleeping\n", getpid());
+        sleep(s);
+        printf("Child %i woke up after %i seconds\n", getpid(), s);
+        //perror("error: ");
+        return s;
+    }
 
     return 0;
 }
